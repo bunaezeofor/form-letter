dd6b22b15d00c2aa30bbc2199f947ae7acb409ed
Buna Ezeofor and Guy Anderson <buna@>
2017-04-26 11:32:06 -0700

    add a second branch. finished adding javascript for form input

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
-$(document).ready(function(){
-  $('#blanks form').submit(funtion(event) {
-      $('.person1').append('hello');
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    var person1Input = $("input#person1").val();
+    var person2Input = $("input#person2").val();
+    $(".person1").append(person1Input);
+    $(".person2").append(person2Input);
 
-      $('#story').show();
 
-      event.preventDefault();
+    $("#story").show();
+
+    event.preventDefault();
   });
 });
